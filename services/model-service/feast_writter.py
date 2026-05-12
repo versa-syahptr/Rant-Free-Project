eebb0cae809e9c1716f060621f17205d20174bb0
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2026-05-13 02:17:27 +0700

    feat: change how to write in feature store based on docs

--- services/model-service/feast_writter.py
+++ services/model-service/feast_writter.py
@@ -11,6 +11,21 @@ def write_prediction(
         language: str,
         model_version: str,
 ):
+    # DEPRECATED, please use v2!
     # Simulate writing to Feast (or any other feature store)
     time.sleep(0.05)  # Simulate network/database latency
     logger.info(f"Writing prediction to Feast [{request_id}]: text='{text[:30]}...', scores={scores}, confidence={confidence:.4f}, language={language}, model_version={model_version}")
+
+def write_prediction_v2(
+        request_id: str,
+        text: str,
+        score_toxic: float,
+        confidence: float,
+        language: str,
+        model_version: str,
+):
+    toxic = None
+
+    # Simulate writing to Feast (or any other feature store)
+    time.sleep(0.05)  # Simulate network/database latency
+    logger.info(f"Writing prediction to Feast [{request_id}]: text='{text[:30]}...', score_toxic={score_toxic}, confidence={confidence:.4f}, language={language}, model_version={model_version}, toxic={toxic}")
